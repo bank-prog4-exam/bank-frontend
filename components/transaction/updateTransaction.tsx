3c3cf20af52ac1923632bf2b8cc942e333492000
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-26 20:08:11 +0100

    fix : Update for function to update transaction pacial

--- components/transaction/updateTransaction.tsx
+++ components/transaction/updateTransaction.tsx
@@ -54,8 +54,6 @@ export function UpdateTransaction() {
     }, [id]); 
     
     const onSubmit: SubmitHandler<FormValues> = async (data) => {
-        console.log(data);
-        
         try {
             const updatedFormData = { ...data, id };
 
@@ -84,6 +82,15 @@ export function UpdateTransaction() {
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <input type="hidden" {...register("id")} /> 
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Account :
+                <select {...register("idAccount")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="">Select Account</option>
+                    {accounts.map(account => (
+                        <option key={account.id} value={account.id}>{`${account.lastName} ${account.firstName}`}</option>
+                    ))}
+                </select>
+            </label>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Transaction Amount:
                 <input type="number" {...register("transactionAmount")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
